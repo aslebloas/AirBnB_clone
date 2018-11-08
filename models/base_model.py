a9f2089ce0259c9406b81079f2da2e907455baa4
Hunt6666 <489@holbertonschool.com>
2018-11-08 11:52:45 -0800

    init updated

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,31 @@ from datetime import date
 
 class BaseModel():
     """BaseModel Class"""
-    def __init__(self, id=str(uuid.uuid4()), created_at=date.today, updated_at=date.today):
+    def __init__(self, *args, **kwargs):
         """Initialization of instance
         Args:
              created_at: current datetime when an instance is created
              updated_at: current datetime when an instance is created or updated
         """
-        self.id = id
-        self.created_at = str(created_at)
-        self.updated_at = str(updated_at)
+        self.id = str(uuid.uuid4())
+        self.created_at = str(date.today)
+        self.updated_at = str(date.today)
+        if len(kwargs) != 0:
+            """ LOOP FOR SETTING VALUES """
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                if k == "my_number":
+                    self.my_number = v
+                if k == "__class__":
+                    self.__class__ = v
+                if k == "updated_at":
+                    """ NEED TO MAKE THESE DATETIME"""
+                    self.updated_at = v
+                if k == "created_at":
+                    self.created_at = v
+                if k == "name":
+                    self.name = v
 
     def __str__(self):
         """prints a summary of the instance attributes"""
@@ -30,7 +46,7 @@ class BaseModel():
     def to_dict(self):
         """ returns a dictionary containing all keys and values of __dict
         of the instance """
-        #dic = {'id': self.id, '
         dic = self.__dict__
-        dic ['__class__'] = self.__class__
+        if ! dic['class']:
+            dic ['__class__'] = self.__class__
         return dic
