4f8a3f2dc48ad0165d3dc6c5a65a49d367af862b
aslebloas <aslebloas@gmail.com>
2018-11-08 21:03:00 +0000

    update date attribute to the right format and to_dict method with string formats for key class

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,7 @@
 """Module with classe BaseModel"""
 
 import uuid
-from datetime import date
-
+from datetime import datetime
 
 class BaseModel():
     """BaseModel Class"""
@@ -14,24 +13,8 @@ class BaseModel():
              updated_at: current datetime when an instance is created or updated
         """
         self.id = str(uuid.uuid4())
-        self.created_at = str(date.today)
-        self.updated_at = str(date.today)
-        if len(kwargs) != 0:
-            """ LOOP FOR SETTING VALUES """
-            for k, v in kwargs.items():
-                if k == "id":
-                    self.id = v
-                if k == "my_number":
-                    self.my_number = v
-                if k == "__class__":
-                    self.__class__ = v
-                if k == "updated_at":
-                    """ NEED TO MAKE THESE DATETIME"""
-                    self.updated_at = v
-                if k == "created_at":
-                    self.created_at = v
-                if k == "name":
-                    self.name = v
+        self.created_at = str(datetime.today())
+        self.updated_at = str(datetime.today())
 
     def __str__(self):
         """prints a summary of the instance attributes"""
@@ -41,12 +24,12 @@ class BaseModel():
     def save(self):
         """updates the public instance attribute upadated_at
         with the current datetime"""
-        self.updated_at = date.today
+        self.updated_at = str(datetime.today())
 
     def to_dict(self):
-        """ returns a dictionary containing all keys and values of __dict
+        """ returns a dictionary containing all keys and values of __dict__
         of the instance """
         dic = self.__dict__
-        if ! dic['class']:
-            dic ['__class__'] = self.__class__
+        if '__class__' not in dic:
+            dic['__class__'] = str(self.__class__)
         return dic
