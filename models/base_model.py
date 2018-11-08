3d08f15b4315eae685354c0f09e1e9cda1697b94
aslebloas <aslebloas@gmail.com>
2018-11-08 21:41:03 +0000

    Fixed the date formating issue and the class format in kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,27 @@ class BaseModel():
              created_at: current datetime when an instance is created
              updated_at: current datetime when an instance is created or updated
         """
+        fmt = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.today())
-        self.updated_at = str(datetime.today())
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
+        if len(kwargs) != 0:
+            """ LOOP FOR SETTING VALUES """
+
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                if k == "my_number":
+                    self.my_number = v
+                if k == "__class__":
+                    self.__class__ = type(self)
+                if k == "name":
+                    self.name = v
+                if k == "updated_at":
+                    """ NEED TO MAKE THESE DATETIME"""
+                    self.updated_at = datetime.strptime(v, fmt)
+                if k == "created_at":
+                    self.created_at = datetime.strptime(v, fmt)
 
     def __str__(self):
         """prints a summary of the instance attributes"""
@@ -24,7 +42,7 @@ class BaseModel():
     def save(self):
         """updates the public instance attribute upadated_at
         with the current datetime"""
-        self.updated_at = str(datetime.today())
+        self.updated_at = datetime.utcnow().isoformat()
 
     def to_dict(self):
         """ returns a dictionary containing all keys and values of __dict__
