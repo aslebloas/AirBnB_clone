995d3b852c70fabf73514ced692c81c64d05bed4
Hunt6666 <489@holbertonschool.com>
2018-11-11 16:35:50 -0800

    all on consol works right wooo hoooo

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 
 import cmd, sys, os, json
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,48 +20,42 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         elif len(args) == 1:
             print("** instance id missing **")
-            if os.path.exists('file.json'):
-                file_path = os.path.abspath("file.json")
-                with open(file_path, "r+") as file:
-                    dic = json.load(file)
-                    flag = 1
-                    for k, v in dic:
-                        key = k.split('.')
-                        if key[1] == args[2]:
-                            flag = 0
-                            if len(args) == 2:
-                                print("** attribute name missing **")
-                            elif len(args) == 3:
-                                print("** value missing **")
-                            else:
-                                """ actual update"""
-                                v[args[2]] = args[3]
-                                json.dump(dic, file)
-                    if flag == 1:
-                        print("** no instance found **")
+        else:
+            dic = storage.all()
+            flag = 1
+            for k, v in dic:
+                key = k.split('.')
+                if key[1] == args[2]:
+                    flag = 0
+                    if len(args) == 2:
+                        print("** attribute name missing **")
+                    elif len(args) == 3:
+                        print("** value missing **")
+                    else:
+                        """ actual update"""
+                        v[args[2]] = args[3]
+                        v.save()
+                if flag == 1:
+                    print("** no instance found **")
 
     def do_show(self, line):
         """Shows a given Model"""
         models = ['BaseModel']
         """ Need Work once more models made"""
-        if not line:
+        if len(line) == 0:
             print("** class name missing **")
         else:
             args = line.split(' ')
             if args[0] in models:
                 if len(args) < 2:
                     print("** instance id missing **")
-                elif os.path.exists('file.json'):
-                    file_path = os.path.abspath("file.json")
-                    with open(file_path, "r") as file:
-                        dic = json.load(file)
-                        ins_id = args[0] + '.' + args[1]
-                        if ins_id in dic:
-                            print(dic[ins_id])
-                        else:
-                            print("** no instance found **")
                 else:
-                    print("** no instance found no file**")
+                    dic = storage.all()
+                    ins_id = args[0] + '.' + args[1]
+                    if ins_id in dic:
+                        print(dic[ins_id])
+                    else:
+                        print("** no instance found **")
             else:
                 print("** class doesn't exist **")
 
@@ -69,18 +64,16 @@ class HBNBCommand(cmd.Cmd):
         models = ["BaseModel"]
         """Add More Once more Models added"""
         if line in models:
-            if os.path.exists('file.json'):
-                file_path = os.path.abspath("file.json")
-                with open(file_path, "r") as file:
-                    dic = json.load(file)
-                    lst = []
-                    for k, v in dic:
-                        key = k.split('.')
-                        if line == key[0]:
-                            """ Needs work once mode models added"""
-                            Model = BaseModel(v)
-                            lst += [str(Model)]
-                    print(lst)
+            dic = storage.all()
+            lst = []
+            for k in dic.keys():
+                key = k.split('.')
+                if line == key[0]:
+                    """ Needs work once mode models added"""
+                    st = ('[' + line + '] (' + str(dic[k]['id']) + ') '
+                          + str(dic[k]))
+                    lst += [st]
+            print(lst)
         else:
             print("** class doesn't exist **")
 
@@ -96,28 +89,20 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) < 2:
             print("** instance id missing **")
         else:
-            file_path = os.path.abspath("file.json")
-            if os.path.exists('file.json'):
-                with open(file_path, "r+") as file:
-                    dic = json.loads(file)
-                    inst = args[0] + '.' + args[1]
-                    if inst in dic:
-                        dic.pop(inst)
-                        json.dump(dic, file)
-                    else:
-                        print("** no instance found **")
+            dic = storage.all()
+            inst = args[0] + '.' + args[1]
+            if inst in dic:
+                dic.pop(inst)
+                storage.save()
+            else:
+                print("** no instance found **")
 
     def do_create(self, line):
         """Creates a new Model"""
         if line == "BaseModel":
             new = BaseModel()
             print(new.id)
-            file_path = os.path.abspath("file.json")
-            with open(file_path, "r+") as file:
-                dic = json.loads(file)
-                key = line + '.' + str(new.id)
-                dic[key] = new.__dict__
-                json.dump(dic, file)
+            new.save()
         elif len(line) == 0:
             print("** class name missing **")
         else:
