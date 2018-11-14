63df3bd5b700d9c94938a51a178ca84cc7c3aadc
Hunt6666 <489@holbertonschool.com>
2018-11-14 14:09:45 -0800

    bad first

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,4 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """State Class"""
-    def __init__(self, *args, **kwargs):
-        """Initialization of instance"""
-        self.name = ''
-        super(Amenity, self).__init__(**kwargs)
+    name = ''
--- models/city.py
+++ models/city.py
@@ -4,13 +4,5 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """State Class"""
-    def __init__(self, *args, **kwargs):
-        """Initialization of instance"""
-        self.name = ''
-        self.state_id = ''
-        if len(kwargs) != 0:
-            if "name" in kwargs:
-                self.name = kwargs["name"]
-            if "state_id" in kwargs:
-                self.state_id = kwargs["state_id"]
-        super(City, self).__init__(**kwargs)
+    name = ''
+    state_id = ''
